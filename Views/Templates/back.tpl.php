b461d59bf44054768befb74e757ee5434099c2d6
errol <errolfieschi@gmail.com>
2021-02-25 01:21:45 +0100

    add dashboard

--- Views/Templates/back.tpl.php
+++ Views/Templates/back.tpl.php
@@ -1,58 +1,58 @@
 <!DOCTYPE html>
 <html lang="FR">
 <head>
-	<meta charset="UTF-8">
-	<title>Template de BACK</title>
-	<meta name="description" content="ceci est la description de ma page">
+    <meta charset="UTF-8">
+    <title>Template de BACK</title>
+    <meta name="description" content="ceci est la description de ma page">
     <link href="/Content/dist/main.css" rel="stylesheet">
 
 </head>
 <body>
-	<header>
-            <nav class="nav vertical-nav">
-                <div class="nav-brand">
-                    <a href="#">JINGO</a>
-                </div>
-                <ul>
-                    <li class="nav-item">
-                        <img src="/Content/svg/board.svg" alt="icon">
-                        <a class="nav-link" href="#">Tableau de bord</a>
-                    </li>
-                    <li class="nav-item">
-                        <img src="/Content/svg/page.svg" alt="icon">
-                        <a class="nav-link" href="#">Pages</a>
-                    </li>
-                    <li class="nav-item">
-                        <img src="/Content/svg/formation.svg" alt="icon">
-                        <a class="nav-link" href="#">Formations</a>
-                    </li>
-                    <li class="nav-item">
-                        <img src="/Content/svg/lecon.svg" alt="icon">
-                        <a class="nav-link " href="#">Leçons</a>
-                    </li>
-                    <li class="nav-item">
-                        <img src="/Content/svg/article.svg" alt="icon">
-                        <a class="nav-link " href="#">Articles</a>
-                    </li>
-                    <li class="nav-item">
-                        <img src="/Content/svg/user.svg" alt="icon">
-                        <a class="nav-link " href="#">Utilisateurs</a>
-                    </li>
-                    <hr class="nav-separator">
-                    <li class="nav-item">
-                        <img src="/Content/svg/setting.svg" alt="icon">
-                        <a class="nav-link " href="#">Paramètres</a>
-                    </li>
-                    <li class="nav-item">
-                        <img src="/Content/svg/logout.svg" alt="icon">
-                        <a class="nav-link " href="#">Déconnexion</a>
-                    </li>
-                </ul>
-            </nav>
-	</header>
+<div class="container-back">
+    <nav class="nav vertical-nav">
+        <div class="nav-brand">
+            <a href="#">JINGO</a>
+        </div>
+        <ul>
+            <li class="nav-item">
+                <img src="/Content/svg/board.svg" alt="icon">
+                <a class="nav-link" href="#">Tableau de bord</a>
+            </li>
+            <li class="nav-item">
+                <img src="/Content/svg/page.svg" alt="icon">
+                <a class="nav-link" href="#">Pages</a>
+            </li>
+            <li class="nav-item">
+                <img src="/Content/svg/formation.svg" alt="icon">
+                <a class="nav-link" href="#">Formations</a>
+            </li>
+            <li class="nav-item">
+                <img src="/Content/svg/lecon.svg" alt="icon">
+                <a class="nav-link " href="#">Leçons</a>
+            </li>
+            <li class="nav-item">
+                <img src="/Content/svg/article.svg" alt="icon">
+                <a class="nav-link " href="#">Articles</a>
+            </li>
+            <li class="nav-item">
+                <img src="/Content/svg/user.svg" alt="icon">
+                <a class="nav-link " href="#">Utilisateurs</a>
+            </li>
+            <hr class="nav-separator">
+            <li class="nav-item">
+                <img src="/Content/svg/setting.svg" alt="icon">
+                <a class="nav-link " href="#">Paramètres</a>
+            </li>
+            <li class="nav-item">
+                <img src="/Content/svg/logout.svg" alt="icon">
+                <a class="nav-link " href="#">Déconnexion</a>
+            </li>
+        </ul>
+    </nav>
+
 
     <?php include $this->view; ?>
-
+</div>
     <script type="text/javascript" src="https://kit.fontawesome.com/19c1e7b3bd.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- Views/dashboard.view.php
+++ Views/dashboard.view.php
@@ -1,3 +1,90 @@
-<section>
-    <h2>Vue Dashboard</h2>
-</section>
+<div class="container-back-wrap">
+    <section>
+        <div class="banner banner--text" style="background-image: url('https://i.pinimg.com/originals/26/ae/12/26ae1241ca65ba8e8ff4a4d442c92566.png'); height: 100px;
+        color: white;
+        font-weight: bold;
+        font-size: 1.5rem;
+        border-radius: .3em;
+">
+            <div class="bg">
+                <p>DASHBOARD</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-4">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du tloremext pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-4">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-4">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card-center card--shadow">
+                    <div class="card-body">
+                        <img src="/Content/Images/register.svg" alt="register">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
