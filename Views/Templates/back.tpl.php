13779e7bcbace9a60d0cc51966f785e291d07549
errol <errolfieschi@gmail.com>
2021-02-25 18:12:34 +0100

    add path training

--- Views/Templates/back.tpl.php
+++ Views/Templates/back.tpl.php
@@ -37,7 +37,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="/training">
                 <img src="/Content/svg/formation.svg" alt="icon">
                     <span>Formations</span>
                 </a>
