7a5b3caaeb6044efe2a7343c1c9aad2877c653e3
errol <errolfieschi@gmail.com>
2021-02-25 11:20:01 +0100

    fixed-grid size

--- Views/dashboard.view.php
+++ Views/dashboard.view.php
@@ -11,38 +11,38 @@
         <div class="row">
             <section class="col-lg-7">
                 <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                    <div class="col-md-6 col-sm-12">
                         <div class="card-center card--shadow">
                             <img class="svg-dashboard" src="/Content/Images/create_courses.svg" alt="register">
                         </div>
                         <p>Créer un cours</p>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                    <div class="col-md-6 col-sm-12 col-xl-6">
                         <div class="card-center card--shadow">
                             <img class="svg-dashboard" src="/Content/Images/create_lesson.svg" alt="lesson">
                         </div>
                         <p>Créer une leçon</p>
                     </div>
 
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                    <div class="col-md-6 col-sm-12">
                         <div class="card-center card--shadow">
                             <img class="svg-dashboard" src="/Content/Images/create_page.svg" alt="import page">
                         </div>
                         <p>Créer un cours</p>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                    <div class="col-md-6 col-sm-12">
                         <div class="card-center card--shadow">
                             <img class="svg-dashboard" src="/Content/Images/import_file.svg" alt="files">
                         </div>
                         <p>Créer une leçon</p>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                    <div class="col-md-6 col-sm-12">
                         <div class="card-center card--shadow">
                             <img class="svg-dashboard" src="/Content/Images/my_photo.svg" alt="photos">
                         </div>
                         <p>Créer un cours</p>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                    <div class="col-md-6 col-sm-12">
                         <div class="card-center card--shadow">
                             <img class="svg-dashboard" src="/Content/Images/my_video.svg" alt="register">
                         </div>
@@ -52,7 +52,7 @@
             </section>
             <section class="col-lg-5 col-sm-12">
                 <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
+                    <div class="col-sm-12">
                         <div class="card card--shadow" style="height: 710px;">
                             <div class="card-body">
                                 <p>Ceci est du text pour entrer à
