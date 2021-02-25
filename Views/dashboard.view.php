d00a14be9bb4857512a7d4eeaee102273dd1fc00
errol <errolfieschi@gmail.com>
2021-02-25 11:13:21 +0100

    grid fixed, dashboard lg-md finish, add banner

--- Views/dashboard.view.php
+++ Views/dashboard.view.php
@@ -1,86 +1,79 @@
 <div class="container-back-wrap">
     <section>
-        <div class="banner banner--text" style="background-image: url('https://i.pinimg.com/originals/26/ae/12/26ae1241ca65ba8e8ff4a4d442c92566.png'); height: 100px;
-        color: white;
-        font-weight: bold;
-        font-size: 1.5rem;
-        border-radius: .3em;
-">
+        <div class="banner banner--text banner--header" style="background-image: url('https://i.pinimg.com/originals/26/ae/12/26ae1241ca65ba8e8ff4a4d442c92566.png');">
             <div class="bg">
-                <p>DASHBOARD</p>
+                <h4>Tableau de bord</h4>
+                <p>L'endroit pour configurer ton site comme tu le souhaites</p>
             </div>
         </div>
     </section>
     <section>
         <div class="row">
-            <div class="col-4">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
+            <section class="col-lg-7">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                        <div class="card-center card--shadow">
+                            <img class="svg-dashboard" src="/Content/Images/create_courses.svg" alt="register">
+                        </div>
+                        <p>Créer un cours</p>
                     </div>
-                </div>
-            </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                        <div class="card-center card--shadow">
+                            <img class="svg-dashboard" src="/Content/Images/create_lesson.svg" alt="lesson">
+                        </div>
+                        <p>Créer une leçon</p>
+                    </div>
 
-            <div class="col-4">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                        <div class="card-center card--shadow">
+                            <img class="svg-dashboard" src="/Content/Images/create_page.svg" alt="import page">
+                        </div>
+                        <p>Créer un cours</p>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                        <div class="card-center card--shadow">
+                            <img class="svg-dashboard" src="/Content/Images/import_file.svg" alt="files">
+                        </div>
+                        <p>Créer une leçon</p>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                        <div class="card-center card--shadow">
+                            <img class="svg-dashboard" src="/Content/Images/my_photo.svg" alt="photos">
+                        </div>
+                        <p>Créer un cours</p>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                        <div class="card-center card--shadow">
+                            <img class="svg-dashboard" src="/Content/Images/my_video.svg" alt="register">
+                        </div>
+                        <p>Créer une leçon</p>
                     </div>
                 </div>
-            </div>
+            </section>
+            <section class="col-lg-5 col-sm-12">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
+                        <div class="card card--shadow" style="height: 710px;">
+                            <div class="card-body">
+                                <p>Ceci est du text pour entrer à
+                                    l'interieur de ma card !</p>
+                                <a href="#" class="button">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
 
-            <div class="col-4">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du tloremext pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
     <section>
         <div class="row">
-            <div class="col-4">
+            <div class="col-lg-3">
                 <div class="card card--shadow">
                     <div class="card-body">
                         <p>Ceci est du text pour entrer à
                             l'interieur de ma card !</p>
                         <a href="#" class="button">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="row">
-            <div class="col-4">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card-center card--shadow">
-                    <div class="card-body">
-                        <img src="/Content/Images/register.svg" alt="register">
                     </div>
                 </div>
             </div>
