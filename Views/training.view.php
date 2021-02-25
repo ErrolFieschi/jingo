2d371cc34d2ad9290fb711bb2869043c78a6e99a
Alexandre <a-savean@hotmail.fr>
2021-02-25 11:26:59 +0100

    Resolved conflit & begin page formation

---
+++ Views/training.view.php
@@ -0,0 +1,85 @@
+<div class="flex-wrap">
+    <section>
+        <div class="banner banner--text" style="background-image: url('https://i.pinimg.com/originals/26/ae/12/26ae1241ca65ba8e8ff4a4d442c92566.png'); height: 100px;
+        color: white;
+        font-weight: bold;
+        font-size: 1.5rem;
+        border-radius: .3em;
+">
+            <div class="bg">
+                <p>DASHBOARD</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-3">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-12">
+                <div class="card flex-row flex-wrap">
+                    <div class="card-header border-0" style="align-items: stretch;">
+                        <img src="/Content/Images/lesson.png" alt="">
+                    </div>
+                    <div class="card-block px-2">
+                        <h4 class="card-title">Title</h4>
+                        <p class="card-text">Description</p>
+                        <a href="#" class="btn btn-primary">BUTTON</a>
+                    </div>
+                    <div class="w-100" style="width: 100%;"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-12">
+                <div class="card card--shadow">
+                    <div class="card-body">
+                        <p>Ceci est du text pour entrer à
+                            l'interieur de ma card !</p>
+                        <a href="#" class="button">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+</div>
+
