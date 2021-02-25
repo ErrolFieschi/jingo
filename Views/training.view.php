4f079b6b9d2c758e5d18c46a35d2e7fae8b59700
Alexandre <a-savean@hotmail.fr>
2021-02-25 18:01:48 +0100

    Formation page -  List of lesson

--- Views/training.view.php
+++ Views/training.view.php
@@ -1,85 +1,125 @@
-<div class="flex-wrap">
+<div class="container-back-wrap">
     <section>
-        <div class="banner banner--text" style="background-image: url('https://i.pinimg.com/originals/26/ae/12/26ae1241ca65ba8e8ff4a4d442c92566.png'); height: 100px;
-        color: white;
-        font-weight: bold;
-        font-size: 1.5rem;
-        border-radius: .3em;
-">
+        <div class="banner banner--text banner--header" style="background-image: url('https://i.pinimg.com/originals/26/ae/12/26ae1241ca65ba8e8ff4a4d442c92566.png');">
             <div class="bg">
-                <p>DASHBOARD</p>
+                <h4>Mes formations</h4>
+                <p class="my-0">L’endroit pour créer, modifier ou supprimer des cours</p>
+            </div>
+        </div>
+    </section>
+    <!--    Réaliser les inputs thème et barre de recherche-->
+    <!--    <section>-->
+    <!--        <div class="row">-->
+    <!--            <div class="col-12" style="background-color: red;">-->
+    <!--                    -->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </section>-->
+    <section>
+        <div class="row">
+            <div class="col-xl-3 col-md-3 col-sm-12 card--inverse">
+                <div class="card-center card--shadow">
+                    <img class="svg-dashboard--formation" src="/Content/Images/create_courses.svg" alt="register">
+                </div>
+                <p>Ajouter un cours</p>
+            </div>
+            <div class="col-xl-3 col-md-3 col-sm-12 card--inverse">
+                <div class="card-center card--shadow">
+                    <img class="svg-dashboard--formation" src="/Content/Images/create_lesson.svg" alt="lesson">
+                </div>
+                <p>Cours favoris</p>
+            </div>
+
+            <div class="col-xl-3 col-md-3 col-sm-12 card--inverse">
+                <div class="card-center card--shadow">
+                    <img class="svg-dashboard--formation" src="/Content/Images/create_page.svg" alt="import page">
+                </div>
+                <p>Dernier cours modifié</p>
+            </div>
+            <div class="col-xl-3 col-md-3 col-sm-12 card--inverse">
+                <div class="card-center card--shadow">
+                    <img class="svg-dashboard--formation" src="/Content/Images/import_file.svg" alt="files">
+                </div>
+                <p>Cours à la une</p>
             </div>
         </div>
     </section>
     <section>
-        <div class="row">
-            <div class="col-3">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
+        <div class="row mb-4">
+            <div class="col-sm-12">
+                <div class="card flex-row flex-wrap card--shadow justify-content-between">
+                    <div class="card-header">
+                        <img src="/Content/Images/formation.png" alt="title image" style="object-fit: cover;">
+                    </div>
+                    <div class="card-block">
+                        <h4 class="card-title">Prenez en main Bootstrap</h4>
+                        <p class="card-text mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet...
+                        </p>
+                        <a href="#" style="color: #c56f43;">Développement </a>
+                        <a href="#" style="color: #bfbfbf;">| 3 leçons associées</a>
+                    </div>
+                    <div class="card-button">
+                        <div class="card-icon">
+                            <a href="#"><img src="/Content/svg/edit.svg" alt="edit button"></a>
+                        </div>
+                        <div class="card-icon">
+                            <a href="#"><img src="/Content/svg/setting-bis.svg" alt="setting button"></a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-3">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
+        </div>
+        <div class="row mb-4">
+            <div class="col-sm-12">
+                <div class="card flex-row flex-wrap card--shadow justify-content-between">
+                    <div class="card-header">
+                        <img src="/Content/Images/learn.png" alt="title image">
+                    </div>
+                    <div class="card-block">
+                        <h4 class="card-title">Diriger une équipe digitale</h4>
+                        <p class="card-text mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet...
+                        </p>
+                        <a href="#" style="color: #5e1a78;">Digitale </a>
+                        <a href="#" style="color: #bfbfbf;">| 7 leçons associées</a>
+                    </div>
+                    <div class="card-button">
+                        <div class="card-icon">
+                            <a href="#"><img src="/Content/svg/edit.svg" alt="edit button"></a>
+                        </div>
+                        <div class="card-icon">
+                            <a href="#"><img src="/Content/svg/setting-bis.svg" alt="setting button"></a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-3">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
+        </div>
+        <div class="row mb-4">
+            <div class="col-sm-12">
+                <div class="card flex-row flex-wrap card--shadow justify-content-between">
+                    <div class="card-header">
+                        <img src="/Content/Images/courses.png" alt="title image" style="object-fit: cover;">
                     </div>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
+                    <div class="card-block">
+                        <h4 class="card-title">Manager une équipe</h4>
+                        <p class="card-text mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet...
+                        </p>
+                        <a href="#" style="color: #519e1b;">Management </a>
+                        <a href="#" style="color: #bfbfbf;">| 10 leçons associées</a>
+                    </div>
+                    <div class="card-button">
+                        <div class="card-icon">
+                            <a href="#"><img src="/Content/svg/edit.svg" alt="edit button"></a>
+                        </div>
+                        <div class="card-icon">
+                            <a href="#"><img src="/Content/svg/setting-bis.svg" alt="setting button"></a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <section>
-        <div class="row">
-            <div class="col-12">
-                <div class="card flex-row flex-wrap">
-                    <div class="card-header border-0" style="align-items: stretch;">
-                        <img src="/Content/Images/lesson.png" alt="">
-                    </div>
-                    <div class="card-block px-2">
-                        <h4 class="card-title">Title</h4>
-                        <p class="card-text">Description</p>
-                        <a href="#" class="btn btn-primary">BUTTON</a>
-                    </div>
-                    <div class="w-100" style="width: 100%;"></div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="row">
-            <div class="col-12">
-                <div class="card card--shadow">
-                    <div class="card-body">
-                        <p>Ceci est du text pour entrer à
-                            l'interieur de ma card !</p>
-                        <a href="#" class="button">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </section>
 </div>
 
