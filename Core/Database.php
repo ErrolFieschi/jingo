1ae63b2f69c93d5438b90592fff759ff18cef424
alexbaudry0 <alexbaudry0@gmail.com>
2021-04-28 11:17:19 +0200

    Singletone BDD + UPDATE

--- Core/Database.php
+++ Core/Database.php
@@ -4,74 +4,112 @@ namespace App\Core;
 
 class Database
 {
-
-	private $pdo;
+    protected static $_instance = null ;
+	private $pdo ;
 	private $table;
+	protected $bdd ;
 
-	public function __construct(){
+	private function __construct(){
 		try{
-			$this->pdo = new \PDO(DBDRIVER.":dbname=".DBNAME.";host=".DBHOST.";port=".DBPORT,DBUSER,DBPWD);
-
+            $this->pdo =  new \PDO(DBDRIVER.":dbname=".DBNAME.";host=".DBHOST.";port=".DBPORT,DBUSER,DBPWD);
 			if(ENV == "dev"){
-				$this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-	    		$this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+                $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
     		}
-
 		}catch(\Exception $e){
 			die("Erreur SQL " . $e->getMessage());
 		}
 
-		$getCalledClassExploded = explode("\\", get_called_class()); //App\Models\User
-		$this->table = DBPREFIXE.end($getCalledClassExploded);
 	}
+
+    public static function getInstance() {
+	    if(is_null(self::$_instance)) {
+            self::$_instance = new Database() ;
+        }
+	    return self::$_instance ;
+    }
+
+    public function setTable($table) {
+
+        self::$_instance->table = $table ;
+    }
 
 
 	public function save(){
-
-		/*
-		//Prepare
-		$query = $this->pdo->prepare("INSERT INTO wpml_user (firstname, lastname, email) 
-				VALUES ( :firstname , :lastname , :email );");  // 1
-
-		//Executer
-		$query->execute(
-				[
-					"firstname"=>"Yves",
-					"lastname"=>"Skrzypczyk",
-					"email"=>"y.skrzypczyk@gmail.com"
-				]
-		);
-		*/
-
-	
-
 
 		$columns = array_diff_key (
 						get_object_vars($this),
 						get_class_vars(get_class())
 					);
-
-
+		
 		//INSERT OU UPDATE
 		// $id == null -> INSERT SINON UPDATE
 		if( is_null($this->getId()) ){
 			//INSERT
-			$query = $this->pdo->prepare("INSERT INTO ".$this->table." (".
+			$query = $this->bdd->pdo->prepare("INSERT INTO ".$this->bdd->table." (".
 					implode(",", array_keys($columns))
 				.") 
 				VALUES ( :".
 					implode(",:", array_keys($columns))
 				." );");	
 		}else{
-			//UPDATE
+			$sql = "";
+			foreach ($columns as $col => $value) {
+			    $sql.= $col ." = ". $value . "," ;
+            }
 
+            $query = $this->bdd->pdo->prepare("UPDATE ". $this->bdd->table . "SET" .rtrim($sql,",")."WHERE id =" .$this->getId() . ";");
 		}
-
 		$query->execute($columns);
-		
-
 
 	}
+
+
+
+	public function updateOneRow($col, $value) {
+
+	    $query = $this->bdd->pdo->prepare("UPDATE ".$this->bdd->table." SET ".$col." = :value ;");
+
+	    $query->execute([
+            "value" => $value
+        ]);
+
+    }
+
+    public function getAllRow() {
+
+        $query = $this->bdd->pdo->prepare("SELECT * FROM ".$this->bdd->table." ;");
+
+        $query->execute();
+
+        return $query->fetchAll();
+
+    }
+
+    public function getRowWithId($id) {
+
+        $query = $this->bdd->pdo->prepare("SELECT * FROM ".$this->bdd->table." WHERE id = :id ;");
+
+        $query->execute([
+            "id" => $id
+        ]);
+
+        return $query->fetch();
+
+    }
+
+    public function getOneRowWithId($col, $id) {
+
+        $query = $this->bdd->pdo->prepare("SELECT :col FROM ".$this->bdd->table." WHERE id = :id ;");
+
+        $query->execute([
+            "col" => $col,
+            "id" => $id
+        ]);
+
+        return $query->fetch();
+
+    }
 
 }
 
