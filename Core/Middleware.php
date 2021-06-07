7b5852d4fc785f282df00cf5fac0c1deb32e8727
alexbaudry0 <alexbaudry0@gmail.com>
2021-06-07 18:48:23 +0200

    Add Middleware.php - allow us generation URI from lesson / part / formation

---
+++ Core/Middleware.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Core;
+
+
+use App\Models\Lesson;
+
+class Middleware
+{
+    public static function getRouteLesson(Lesson $lesson)
+    {
+
+    }
+
+    public static function getRoutePart(Part $part)
+    {
+
+    }
+
+    public static function getRouteFormation(Formation $formation)
+    {
+
+    }
+
+}
