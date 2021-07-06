e7fc4e6bd56ff1a1752b24e118766f0f8d46fcdf
Alexandre <a-savean@hotmail.fr>
2021-07-06 21:05:34 +0200

    [TINYMCE] - UPDATE VENDOR #2

---
+++ vendor/tinymce/uploadFile.js
@@ -0,0 +1,48 @@
+tinymce.init({
+    mode : "specific_textareas",
+    height: 500,
+    editor_selector : "jingoEditor",
+    relative_urls: false,
+    language: 'fr_FR',
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | image code formatselect | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat | help',
+
+    images_upload_url: '/vendor/tinymce/upload.php',
+
+    images_upload_handler: function (blobInfo, success, failure) {
+        var xhr, formData;
+
+        xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.open('POST', '/vendor/tinymce/upload.php');
+
+        xhr.onload = function() {
+            var json;
+
+            if (xhr.status != 200) {
+                failure('HTTP Error: ' + xhr.status);
+                return;
+            }
+
+            json = JSON.parse(xhr.responseText);
+
+            if (!json || typeof json.location != 'string') {
+                failure('Invalid JSON: ' + xhr.responseText);
+                return;
+            }
+            success(json.location);
+        };
+
+        formData = new FormData();
+        formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+        xhr.send(formData);
+    },
+});
