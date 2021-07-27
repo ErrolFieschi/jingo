b0e2924dce5153cdae2e7ba1ae1db04f4203070f
isoaLuca <andria.m.luca@outlook.fr>
2021-07-27 22:59:30 +0200

    [article] CSS / Lessons / courses / Part

--- Views/Templates/front.tpl.php
+++ Views/Templates/front.tpl.php
@@ -9,6 +9,7 @@ $getNav = Database::customSelectFromATable('navbar', '*');
     <title>Template de FRONT</title>
     <meta name="description" content="ceci est la description de ma page">
     <link href="/Content/dist/main.css" rel="stylesheet"> <!-- temporary will be removed -->
+    <link href="/Content/dist/article.css" rel="stylesheet">
     <script src="/Content/js/jquery-3.5.1.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/d59c1d4dbf.js" crossorigin="anonymous"></script>
--- Views/library.view.php
+++ Views/library.view.php
@@ -1,13 +1,17 @@
 <div class="container">
-    <section class="flex flex-end flex-row h-100 ">
-        <div class="flex flex-col card--shadow w-70 container-front content-container">
+    <section class="content-container-title pl-0 pr-0 h-10-em bck-c-white w-100 tc">
+        <h6 class="mt-0 mb-0 flex pl-40 "> Accueil > Cours > Angular > <?= $title ?> </h6>
+        <h1 class="courses-title "> <?= $title ?> </h1>
+    </section>
+    <section class="flex flex-end flex-row justify-center h-100 ">
+        <div class="bck-c-white flex flex-col card--shadow w-70 container-front content-container p-6">
             <?= $displayCode ?>
         </div>
 
         <div class="w-10 bck-c-grey-2 b-r-10">
             <h4 class="color-white tc"> <?= $title ?> </h4>
             <div class="bck-c-white card--shadow h-100 b-r-bg-bd">
-                <ol class="bck-c-white h-100 b-r-bg-bd">
+                <ol class="bck-c-white b-r-bg-bd">
                     <?php foreach ($data as $lesson): ?>
                         <li id="chapter">
                             <a class="pages_card decoration-none  chap-animation-title change-content" ChapterPart="<?=$lesson['id']?>" onclick="changeContent(this)"> <?=$lesson['title']?> </a>
