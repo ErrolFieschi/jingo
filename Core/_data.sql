390e703eb49bb421a4285dd1ffa86e3f7321e28b
alexbaudry0 <alexbaudry0@gmail.com>
2021-07-28 03:48:44 +0200

    patch data sql installer

--- Core/_data.sql
+++ Core/_data.sql
@@ -107,6 +107,11 @@ create table PREFIXElesson
         foreign key (part_id) references PREFIXEpart (id)
 );
 
-
+INSERT INTO PREFIXEtraining_tag (id, name) VALUES (1, 'Developpement');
+INSERT INTO PREFIXEtraining_tag (id, name) VALUES (2, 'Marketing');
+INSERT INTO PREFIXEtraining_tag (id, name) VALUES (3, 'Communication');
+INSERT INTO PREFIXEtraining_tag (id, name) VALUES (4, 'Sciences');
+INSERT INTO PREFIXEtraining_tag (id, name) VALUES (5, 'Sport');
+INSERT INTO PREFIXEtraining_tag (id, name) VALUES (6, 'Jeux vidÃ©os');
 
 
