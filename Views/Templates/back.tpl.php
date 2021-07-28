d0aa4787a2695d742923fcb0c9520546096692c6
alexbaudry0 <alexbaudry0@gmail.com>
2021-07-28 04:39:14 +0200

    FIN DE TES MORTS

--- Views/Templates/back.tpl.php
+++ Views/Templates/back.tpl.php
@@ -18,7 +18,7 @@
 <div class="container-back">
     <nav class="nav vertical-nav">
         <div class="nav-brand">
-            <a href="/dashboard">JINGO</a>
+            <a href="/page/accueil">JINGO</a>
         </div>
         <ul>
             <li class="nav-item">
